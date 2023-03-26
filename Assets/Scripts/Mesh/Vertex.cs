dc0c0f9aabe90c8fc7aeb9a8dba0458aff54661b
explorer-i@hotmail.com <>
2023-03-26 17:40:29 +0300

    Job System Examples (from Jasper Flick)

---
+++ Assets/Scripts/Mesh/Vertex.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Mathematics;
+
+namespace Meshes
+{
+    public struct Vertex
+    {
+        public float3 position; // 12 bytes
+        public float3 normal; // 12 bytes
+        public float4 tangent; // 16 bytes
+        public float2 texCoord0; // 8 bytes
+    }
+
+    public struct Face
+    {
+
+    }
+
+
+}
