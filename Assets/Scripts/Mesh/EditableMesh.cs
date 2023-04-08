e6e01f745609ee18b56d233634ee45125ebda94d
explorer-i@hotmail.com <>
2023-04-08 10:17:39 +0300

    Add Face Class for bookkeeping

--- Assets/Scripts/Mesh/EditableMesh.cs
+++ Assets/Scripts/Mesh/EditableMesh.cs
@@ -3,6 +3,9 @@ using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Rendering;
 using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using Meshes;
 #nullable enable
@@ -29,7 +32,7 @@ public class EditableMesh : MonoBehaviour
             meshInternal = default;
             Debug.Log($"Vertex count: {CopyFromMesh.vertexCount}");
             GetComponent<MeshFilter>().mesh = meshInternal.CopySetup(CopyFromMesh);
-
+            
         }
         else
         {
@@ -55,7 +58,7 @@ public class EditableMesh : MonoBehaviour
 namespace Meshes
 {
     [StructLayout(LayoutKind.Sequential)]
-    struct Stream0
+    public struct Stream0
     {
         public float3 position; // 12 bytes
         public float3 normal; // 12 bytes
@@ -66,16 +69,17 @@ namespace Meshes
     {
         private Mesh Mesh;
 
-        private Vertex[] _vertices;
-        private Face[] _faces;
+        private List<Vertex> _vertices;
+        private List<Face> _faces;
 
         private int _maxVerts => defaultMaxVerts;
+        private int _vertexCountInternal;
         private int _maxFaces => defaultMaxTris;
 
         private static readonly int defaultMaxVerts = ushort.MaxValue;
         private static readonly int defaultMaxTris = ushort.MaxValue / 3;
 
-        public int VertexCount { get; private set; }
+        public int VertexCount { get => _vertices.Count; }
         public int TriangleCount { get; private set; }
 
         public Vector3[] Vertices
@@ -127,9 +131,10 @@ namespace Meshes
                 MeshUpdateFlags.DontRecalculateBounds | MeshUpdateFlags.DontValidateIndices);
 
             // initialize safe garbage here
-            _vertices = new Vertex[defaultMaxVerts];
-            _faces = new Face[defaultMaxTris];
-            VertexCount = 0;
+            _vertices = new List<Vertex>(defaultMaxVerts);
+            _faces = new List<Face>(defaultMaxTris/3);
+
+            _vertexCountInternal = 0;
             TriangleCount = 0;
 
             var stream0 = meshData.GetVertexData<Stream0>();
@@ -165,35 +170,21 @@ namespace Meshes
             Setup(Mesh);
             for (int i = 0; i < vertices.Length; ++i)
             {
-                //Debug.Log($"idx: {i}\n\tpos: {vertices[i]}\n\tnorm: {normals[i]}\n");
+                Debug.Log($"idx: {i}\n\tpos: {vertices[i]}\n\tnorm: {normals[i]}\n");
                 AddVertex(vertices[i], normals[i], tangents[i], uvs[i]);
             }
             for (int i = 0; i < triangles.Length; i += 3)
             {
-                //Debug.Log($"idx: {i}\n\tindices: {triangles[i]}, {triangles[i+1]}, {triangles[i+2]}\n");
+                Debug.Log($"idx: {i}\n\tindices: {triangles[i]}, {triangles[i+1]}, {triangles[i+2]}\n");
                 AddTriangle(int3(triangles[i], triangles[i + 1], triangles[i + 2]));
             }
             return Mesh;
         }
 
-        /// <summary>
-        /// Get the position of vertices within the mesh, use the indexes to reference the vertices
-        /// </summary>
-        /// <returns>List of indexes of vertices</returns>
-        public Vector3[] GetVertices()
-        {
-            var positions = new Vector3[_vertices.Length];
-            for (int i = 0; i < _vertices.Length; ++i)
-            {
-                positions[i] = _vertices[i].position;
-            }
-            return positions;
-        }
-
         public Vector3[] GetFaces()
         {
             // TODO
-            return new Vector3[0];
+            throw new NotImplementedException { };
         }
 
         /// <summary>
@@ -204,13 +195,16 @@ namespace Meshes
         public void MoveVertex(int index, float3 position)
         {
             // need to abstract real index
-            _vertices[index].position = position;
+            var vert = _vertices[index];
+            vert.position = position;
+            _vertices[index] = vert;
+            //_vertices[index].position = position;
             var single_vert = new Stream0
             {
-                position = _vertices[index].position,
-                normal = _vertices[index].normal,
-                tangent = _vertices[index].tangent,
-                texCoord0 = _vertices[index].texCoord0,
+                position = vert.position,
+                normal = vert.normal,
+                tangent = vert.tangent,
+                texCoord0 = vert.texCoord0,
             };
             NativeArray<Stream0> buffer = new NativeArray<Stream0>(1, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
             buffer[0] = single_vert;
@@ -239,26 +233,15 @@ namespace Meshes
         /// <returns>index of the vertex</returns>
         public int AddVertex(float3 position, float3 normal, float4 tangent, float2 texCoord0)
         {
-            var vert = new Vertex
-            {
-                position = position,
-                normal = normal,
-                tangent = tangent,
-                texCoord0 = texCoord0
-            };
-            var vert_int = new Stream0
-            {
-                position = vert.position,
-                normal = vert.normal,
-                tangent = vert.tangent,
-                texCoord0 = vert.texCoord0,
-            };
-            _vertices[VertexCount] = vert;
+            var vert = Vertex.Create(position, normal, tangent, texCoord0);
+            vert.indices.Add(_vertexCountInternal);
+            _vertices.Add(vert);
+            Stream0 vert_int = (Stream0)vert;
             NativeArray<Stream0> buffer = new NativeArray<Stream0>(1, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
             buffer[0] = vert_int;
-            Mesh.SetVertexBufferData<Stream0>(buffer, 0, VertexCount, 1);
+            Mesh.SetVertexBufferData<Stream0>(buffer, 0, _vertexCountInternal, 1);
             buffer.Dispose();
-            VertexCount++;
+            _vertexCountInternal++;
             return VertexCount - 1;
         }
 
@@ -278,16 +261,17 @@ namespace Meshes
 
             // copy all of the updated values to the 
 
+            throw new NotImplementedException { };
         }
 
         public void DeleteFace()
         {
-
+            throw new NotImplementedException { };
         }
 
         public void SetFace()
         {
-
+            throw new NotImplementedException { };
         }
 
         /// <summary>
@@ -315,9 +299,17 @@ namespace Meshes
         /// <param name="vertices"></param>
         public void AddFace(int4 vertices)
         {
-            NativeArray<int3> buffer = new NativeArray<int3>(2, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
-            buffer[0] = int3(vertices[0], vertices[1], vertices[2]);
-            buffer[1] = int3(vertices[2], vertices[3], vertices[0]);
+            List<Vertex> face_verts = new List<Vertex> {
+                _vertices[vertices.x],
+                _vertices[vertices.y],
+                _vertices[vertices.z],
+                _vertices[vertices.w]
+            };
+            var face = new Face(face_verts);
+            _faces.Add(face);
+            var buf = face.ToStream();
+            var buffer = new NativeArray<int3>(buf, Allocator.Temp);
+
             Mesh.SetIndexBufferData<int3>(buffer, 0, TriangleCount, 2);
             buffer.Dispose();
             TriangleCount += 2;
@@ -325,8 +317,15 @@ namespace Meshes
 
         public void AddTriangle(int3 vertices)
         {
-            NativeArray<int3> buffer = new NativeArray<int3>(1, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
-            buffer[0] = vertices;
+            List<Vertex> face_verts = new List<Vertex> {
+                _vertices[vertices.x],
+                _vertices[vertices.y],
+                _vertices[vertices.z]
+            };
+            var face = new Face(face_verts);
+            _faces.Add(face);
+            var buf = face.ToStream();
+            var buffer = new NativeArray<int3>(buf, Allocator.Temp);
             Mesh.SetIndexBufferData<int3>(buffer, 0, TriangleCount, 1);
             buffer.Dispose();
             TriangleCount += 1;
@@ -334,18 +333,55 @@ namespace Meshes
 
         public void Extrude()
         {
-
+            throw new NotImplementedException { };
         }
 
+        public Vertex? FindByPosition()
+        {
+            throw new NotImplementedException { };
+        }
     }
 
-    public struct Vertex
+    public class Vertex
     {
         public float3 position; // 12 bytes
         public float3 normal; // 12 bytes
         public float4 tangent; // 16 bytes
         public float2 texCoord0; // 8 bytes
         // maybe add bookkeeping fields?
+        public List<int> indices = new List<int>();
+        public List<Face> faces = new List<Face>();
+
+        public static Vertex Create(float3 position, float3 normal, float4 tangent, float2 texCoord0)
+        {
+            return new Vertex
+            {
+                position = position,
+                normal = normal,
+                tangent = tangent,
+                texCoord0 = texCoord0,
+                indices = new List<int>(),
+            };
+        }
+
+        public static explicit operator Stream0(Vertex vert)
+        {
+            return new Stream0
+            {
+                position = vert.position,
+                normal = vert.normal,
+                tangent = vert.tangent,
+                texCoord0 = vert.texCoord0,
+            };
+        }
+
+        public int GetIndex(Face face)
+        {
+            // TODO: smarter handling
+            return indices.First();
+            //return faces.IndexOf(face);
+        }
+
     }
 
     public struct Edge
@@ -353,8 +389,53 @@ namespace Meshes
 
     }
 
-    public struct Face
+    public class Face
     {
-        public int3 triangle;
+        // Assume that the list of vertices is clockwise on the face
+        public List<Vertex> vertices = new List<Vertex>(4);
+        //public float3 normal;
+
+        private static readonly int3[] empty = new int3[0];
+        private static readonly int3[] degenerate = { int3(0, 0, 0) };
+
+        public static int3[] Empty => empty;
+
+        public Face()
+        {
+
+        }
+
+        public Face(List<Vertex> vertices)
+        {
+            this.vertices = vertices;
+        }
+
+        public void AddVertex()
+        {
+            throw new NotImplementedException { };
+        }
+
+        
+
+        /// <summary>
+        /// Triangulate the face and return the triangle indices
+        /// </summary>
+        /// <returns>Triangle indices</returns>
+        public int3[] ToStream()
+        { 
+            if (vertices.Count < 3)
+            {
+                return empty;
+            } else
+            {
+                var indices = new int3[vertices.Count - 2];
+                var ind0 = vertices[0].GetIndex(this);
+                for (int i = 0; i < vertices.Count - 2; i++)
+                {
+                    indices[i] = int3(ind0, vertices[i + 1].GetIndex(this), vertices[i + 2].GetIndex(this));
+                }
+                return indices;
+            }
+        }
     }
 }
