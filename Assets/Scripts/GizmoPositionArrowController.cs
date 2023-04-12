e4c1dbe57ad8b06bbcd6dc8720cd770d3b69f91e
mkucuk18@ku.edu.tr <>
2023-04-12 17:11:03 +0300

    Compelete move implemantation for whole object

--- Assets/Scripts/GizmoPositionArrowController.cs
+++ Assets/Scripts/GizmoPositionArrowController.cs
@@ -5,12 +5,16 @@ public class GizmoPositionArrowController : MonoBehaviour
     public GameObject xArrow, yArrow, zArrow, xSquare, ySquare, zSquare;
 
     bool xArrowSelected, yArrowSelected, zArrowSelected;
+    bool xSquareSelected, ySquareSelected, zSquareSelected;
     GameObject parent;
 
 
     public bool XArrowSelected { get => xArrowSelected; set => xArrowSelected = value; }
     public bool YArrowSelected { get => yArrowSelected; set => yArrowSelected = value; }
     public bool ZArrowSelected { get => zArrowSelected; set => zArrowSelected = value; }
+    public bool XSquareSelected { get => xSquareSelected; set => xSquareSelected = value; }
+    public bool YSquareSelected { get => ySquareSelected; set => ySquareSelected = value; }
+    public bool ZSquareSelected { get => zSquareSelected; set => zSquareSelected = value; }
 
     private void Start()
     {
@@ -45,6 +49,33 @@ public class GizmoPositionArrowController : MonoBehaviour
             ySquare.SetActive(false);
             zSquare.SetActive(false);
             parent.transform.position = zArrow.transform.position;
+        }
+        else if (xSquareSelected)
+        {
+            xArrow.SetActive(false);
+            yArrow.SetActive(false);
+            zArrow.SetActive(false);
+            ySquare.SetActive(false);
+            zSquare.SetActive(false);
+            parent.transform.position = xSquare.transform.position;
+        }
+        else if (ySquareSelected)
+        {
+            xArrow.SetActive(false);
+            yArrow.SetActive(false);
+            zArrow.SetActive(false);
+            xSquare.SetActive(false);
+            zSquare.SetActive(false);
+            parent.transform.position = ySquare.transform.position;
+        }
+        else if (zSquareSelected)
+        {
+            xArrow.SetActive(false);
+            yArrow.SetActive(false);
+            zArrow.SetActive(false);
+            ySquare.SetActive(false);
+            xSquare.SetActive(false);
+            parent.transform.position = zSquare.transform.position;
         }
         else
         {
