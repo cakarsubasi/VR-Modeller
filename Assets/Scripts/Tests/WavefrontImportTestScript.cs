5b4e00f6917c99609f92a6aa3da1131778479126
explorer-i@hotmail.com <>
2023-04-28 13:19:03 +0300

    Initial commit for Wavefront obj support

---
+++ Assets/Scripts/Tests/WavefrontImportTestScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+using Unity.Mathematics;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Meshes;
+using static Unity.Mathematics.math;
+
+public class WavefrontImportTestScript
+{
+
+
+
+    public void RegexTest()
+    {
+
+    }
+}
