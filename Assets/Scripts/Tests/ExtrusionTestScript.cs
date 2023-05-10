cbabe42799732ef22407d4cbbf4596e3a19f8130
explorer-i@hotmail.com <>
2023-05-10 14:02:05 +0300

    Fix compilation errors due to lack of initialization function.

--- Assets/Scripts/Tests/ExtrusionTestScript.cs
+++ Assets/Scripts/Tests/ExtrusionTestScript.cs
@@ -33,6 +33,32 @@ public class ExtrusionTestScript
         return mesh;
     }
 
+    public UMesh CreateCube()
+    {
+        UMesh mesh = UMesh.Create();
+
+        Vertex v1 = mesh.CreateVertex(float3(-1f, -1f, -1f));
+        Vertex v2 = mesh.CreateVertex(float3(1f, -1f, -1f));
+        Vertex v3 = mesh.CreateVertex(float3(1f, -1f, 1f));
+        Vertex v4 = mesh.CreateVertex(float3(-1f, -1f, 1f));
+
+        Vertex v5 = mesh.CreateVertex(float3(-1f, 1f, -1f));
+        Vertex v6 = mesh.CreateVertex(float3(1f, 1f, -1f));
+        Vertex v7 = mesh.CreateVertex(float3(1f, 1f, 1f));
+        Vertex v8 = mesh.CreateVertex(float3(-1f, 1f, 1f));
+
+        mesh.CreateQuad(new QuadElement<Vertex>(v1, v2, v3, v4));
+        mesh.CreateQuad(new QuadElement<Vertex>(v8, v7, v6, v5));
+
+        mesh.CreateQuad(new QuadElement<Vertex>(v1, v4, v8, v5));
+        mesh.CreateQuad(new QuadElement<Vertex>(v1, v5, v6, v2));
+
+        mesh.CreateQuad(new QuadElement<Vertex>(v2, v6, v7, v3));
+        mesh.CreateQuad(new QuadElement<Vertex>(v3, v7, v8, v4));
+
+        return mesh;
+    }
+
     /// <summary>
     /// From one vertex, extrude a single vertex
     /// Should end with 2 vertices, 1 edge, 0 faces
